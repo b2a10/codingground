cfcaa0dd18b616b65fbbb340b4fac325923f93cc
b2a10 <bbbruinsfan96@gmail.com>
2016-07-22 15:24:18 -0400

    Create ex31.py

---
+++ New Project-20160719/ex31.py
@@ -0,0 +1,54 @@
+print "You enter a dark room with two doors. Do you go through door #1 or door  #2?"
+
+door = raw_input("> ")
+
+if door == "1":
+    print "There's a giant bear here eating a cheese cake. What do you do?"
+    print "1. Take the cake."
+    print "2. Scream at the bear."
+    
+    bear = raw_input("> ")
+    
+    if bear == "1":
+        print "The bear eats your face off. Good job!"
+    elif bear == "2":
+        print "The beat eats your legs off. Good job!"
+    else:
+        print "Well, doing %s is probably better. Bear runs away." % bear
+elif door == "2":
+    print "You stare into the endless abyss at Cthulhu's retinat."
+    print "1. Blueberries."
+    print "2. Yellow jacket clothespins."
+    print "3. Understanding revolvers yelling melodies."
+    
+    insanity = raw_input("> ")
+    
+    if insanity == "1" or insanity == "2":
+        print "Your body survives powered by a mind of jello. Good job!"
+    else:
+        print "The insanity rots your eyes into a pool of muck. Good job!"
+else: 
+    print "You stumble around and fall on a knife and die. Good job!"
+
+
+# Study Drill - Mini Game
+print "~Ben's Mini Game!~"
+
+if door == "1":
+    if bear == "1":
+        print "Looks like you just got your face eaten off. Sorry \'bout that!"
+    else:
+        print "Ouch. Your legs... Sorry!"
+else:
+    print "You are certifiably insane!"
+
+print "Anyways, we have one simple question."
+print "What is the difference between a lamp and a drawer?"
+
+diff = raw_input("> ")
+
+if diff == "Nothing":
+    print "Holy mother of pearl, you got it right! Have a cookie."
+else:
+    print "You are sentenced to year of Jabba the Hutt palace."
+
