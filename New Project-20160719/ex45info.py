8508c85c53ca83a877c6024f5a0a0628b018f5b4
b2a10 <bbbruinsfan96@gmail.com>
2016-08-04 15:06:52 -0400

    Created by Coding Ground Client

---
+++ New Project-20160719/ex45info.py
@@ -0,0 +1,56 @@
+
+fundraisers = {
+    1:"Lemonade Stand",
+    2:"5k Run/Walk",
+    3:"Uptown Bowling Trip",
+    4:"Golf Tournament",
+    5:"50/50 Raffle",
+    6:"Count Items in a Jar",
+    7:"Work-A-Thon",
+    8:"Pro Sports Game"
+}
+
+initiatives = {
+    1:"Street Cleanup",
+    2:"Block Parties",
+    3:"Political Debates",
+    4:"Neighborhood Meetings",
+    5:"Community Dog/Stroller Walks",
+    6:"Homeowners Association",
+    7:"Local Talent Week",
+    8:"Community / Police Positive Reinforcement"
+}
+
+builds = {
+    1:"Build Park",
+    2:"Build Bike Path",
+    3:"Build Community House",
+    4:"Build Restaurant",
+    5:"Build Entertainment Center (arts, music, etc.)",
+    6:"Build Museum/Tourist Center",
+    7:"Buy Land",
+    8:"Build Shopping Plaza/Stadium",
+}
+
+costs = {
+    "Build Park":100,
+    "Build Bike Path":100,
+    "Build Community House":300,
+    "Build Restaurant":300,
+    "Build Entertainment Center (arts, music, etc.)":600,
+    "Build Museum/Tourist Center":500,
+    "Buy Land":800,
+    "Build Shopping Plaza/Stadium":1000
+}
+
+
+
+
+
+
+
+
+
+
+
+
